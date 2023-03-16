82610e02977858e9ee176bcbf17065ea9baf9bcc
capricia <capriciaw16@gmail.com>
2023-03-15 21:37:18 -0500

    coding

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -3,13 +3,8 @@ const categoryRoutes = require("./category-routes.js")
 const productsRoutes = require("./product-routes.js")
 const tagRoutes = require("./tag-routes.js")
 
-router.use("/category",categoryRoutes);
+router.use("/categories",categoryRoutes);
 router.use("/product",productsRoutes);
 router.use("/tag",tagRoutes);
 
-
-router.use((req, res) => {
-    res.send("<h1>Wrong Route!</h1>");
-  });
-  
   module.exports = router;
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,29 +4,44 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-router.get('/', async(req, res) => {
- 
+router.get('/', async (req, res) => {
+    try {
+        const allProducts = await Products.findAll({
+            
+        })
+        console.log()
+        res.json(allProducts) 
+        }
+    catch (err) {
+            res.status(500).json(err)
+        }
+    }
 });
 
 // get one product
-router.get('/:id', async(req, res) => {
- 
+router.get('/:id', async (req, res) => {
+
 });
 
 
 // create new product
-router.post('/', async(req, res) => {
-  
-    
+router.post('/', async (req, res) => {
+    try {
+        const result = await Product.create(req.body)
+        res.json(result)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
 });
 
 // update product
 router.put('/:id', async (req, res) => {
 
-      
+
 });
 
 
-  
+
 
 module.exports = router;

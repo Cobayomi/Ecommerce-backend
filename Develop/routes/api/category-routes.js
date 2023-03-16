ca0c24d0d589a5f26b9a1627711ec87e2e5973ec
capricia <capriciaw16@gmail.com>
2023-03-16 01:18:11 -0500

    cat route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,68 @@ const { Category, Product } = require('../../models');
 
 
 router.get('/', async (req, res) => {
-  
+    try {
+        const categoriesData = await Category.findAll({
+            include: [{ model: Product }]
+        });
+        res.status(200).json(categoriesData);
+    }
+    catch (err) {
+        res.status(404).json(err)
+    }
 });
 
-router.get('/:id', async(req, res) => {
+router.get('/:id', async (req, res) => {
+    try {
+        const onecategory = await Category.findByPk(req.params.id, {
+            include: [{ model: Product }]
+        });
 
-  
+        if (!onecategory) {
+            res.status(500).json({ message: "Category id not found!" });
+            return;
+        }
+        res.status(200).json(onecategory);
+    } catch (err) {
+        res.send(500).json(err);
+    }
 });
 
-
-
-router.post('/', (req, res) => {
- 
+router.post('/', async (req, res) => {
+    try {
+        const createcategory = await Category.create(req.body);
+        return res.status(200).json(createcategory);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
-router.put('/:id', (req, res) => {
- 
+router.put('/:id', async (req, res) => {
+    try {
+        const result = await Category.update(req, body, {
+            where: {
+                id: req.params.id,
+            },
+        })
+        res.json(result)
+    }
+    catch (err) {
+        res.status(400).json(err);
+    }
+
 });
-    
-router.delete('/:id', async(req, res) => {
-  
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const result = await Category.destory({
+            where: {
+                id: req.params.id
+            },
+        });
+        res.json(result);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 
